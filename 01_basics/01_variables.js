19916763c063559acfd36ef5846bf5dd7a0a3caa
ajaypankaj <ajaypankaj777@gmail.com>
2025-04-06 15:17:27 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=556677
+let accountnGmail="ajay@google.com"
+var accountPassword="12345"
+accountCity="Kota"//not good 
+let accountState;
+//accountId=2 //not allowed
+
+
+accountEmail="hc@hc.com"
+accountPassword="1122334455"
+accountCity="Bengaluru"
+/*Prefer not to use var because
+of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) // we are  using nodejs,not browser
+
+//for documamentation check sites like tc39.es and alert mdn
+//https://tc39.es/ecma262/#sec-comments
+
+
+console.log( 3 + 3) // code readabilitty shoud be high
+
+console.log("Ajay")
+
+let name="Ajay"
+let age=21
+let isLoggedIn=false
+
+// primitive data types
+// number => 2 to power 53
+// bigint
+//strinng => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "ajay"); // string
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
