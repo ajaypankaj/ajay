f7ad52b78145a804992fcb436994e08fe6459760
ajaypankaj <ajaypankaj777@gmail.com>
2025-04-06 16:31:53 +0000

    data type summray

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,6 +20,37 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber );
+//console.log(stringNumber);
+//console.log(typeof stringNumber );
+
+
+// ********************* Operations ***************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+//console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+
+let str1 ="hello"
+let str2 =" ajay"
+let str3 =str1 + str2
+//console.log(str3); //  => hello ajay (output)
+
+//console.log("1" + 2); // => 12
+//console.log(1 + "2"); // => 12
+//console.log("1" + 2 + 2); // => 122
+//console.log(1 + 2 + "2"); // => 32
+//don't use this confusing instead use paratheneses
+
+
+
 
